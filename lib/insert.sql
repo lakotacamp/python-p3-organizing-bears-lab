b561ec8fea12721e0907b2a227f7e78d33bee100
lakota <lakotajcamp@gmail.com>
2023-12-21 20:11:10 -0500

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Mr. Chocolate", 1, "M", "Brown", "Sassy", 1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Rowdy",2,"F","Black","Calm",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Tabitha",3,"F","Blue","Prissy",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Sergeant Brown",4,"M","Brown","Serious",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Melissa",5,"F","Polar","Cold",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Grinch",6,"M","Green","Grinchy",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Wendy", 7, "F", "Red", "Hungry",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES (NULL,8,"M","Mystery","Mysterious",1);
